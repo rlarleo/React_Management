7ba980b13f3c648c52851fd809ab7409b7acce56
김기대 <soulen2403@naver.com>
2021-06-25 15:41:27 +0900

    Apply Material UI

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 class Customer extends React.Component {
     render() {
         return (
-            <div>
-                <h2>{this.props.name}</h2>
-                <p>{this.props.birthday}</p>
-                <p>{this.props.gender}</p>
-                <p>{this.props.job}</p>
-            </div>
+            <TableRow>
+                <TableCell>{this.props.id}</TableCell>
+                <TableCell><img src={this.props.image} alt="profile"/></TableCell>
+                <TableCell>{this.props.name}</TableCell>
+                <TableCell>{this.props.birthday}</TableCell>
+                <TableCell>{this.props.gender}</TableCell>
+                <TableCell>{this.props.job}</TableCell>
+            </TableRow>
         )
     }
 }
-class CustomerProfile extends React.Component{
+
+/*class CustomerProfile extends React.Component{
     render(){
         return(
             <div>
@@ -33,5 +38,6 @@ class CustomerInfo extends React.Component {
             </div>
         )
     }
-}
+}*/
+
 export default Customer;
